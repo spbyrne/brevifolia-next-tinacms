82dcbda954e646bf55109876acf9ee4df7520b46
James O'Halloran <james.p.ohalloran@gmail.com>
2020-01-17 16:29:04 -0400

    register teams

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-const express = require('express')
-const next = require('next')
-const cors = require('cors')
-const gitApi = require('@tinacms/api-git')
+const express = require("express");
+const next = require("next");
+const cors = require("cors");
+const gitApi = require("@tinacms/api-git");
+const teams = require("@tinacms/teams");
+const cookieParser = require("cookie-parser");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ 
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({
   dev,
-  dir: './src'
- })
-const handle = app.getRequestHandler()
+  dir: "./src"
+});
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express()
+  const server = express();
 
-  server.use(cors())
-  server.use('/___tina', gitApi.router())
+  server.use(cors());
+  server.use(cookieParser());
+  server.use(teams.router());
+  server.use("/___tina", gitApi.router());
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
 
   server.listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
