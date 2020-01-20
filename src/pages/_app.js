c78cd0efa1d46aa5bd9ad92f7b2c7df838f4566f
James O'Halloran <james.p.ohalloran@gmail.com>
2020-01-20 10:45:15 -0400

    wait for window to register git api

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,27 @@ import React from "react";
 import App from "next/app";
 import { Tina, TinaCMS } from "tinacms";
 import { GitClient } from "@tinacms/git-client";
+import { useEffect } from "react";
 
 class MyApp extends App {
   constructor() {
     super();
     this.cms = new TinaCMS();
+  }
 
+  render() {
+    return <_App cms={this.cms} {...this.props} />;
+  }
+}
+export default MyApp;
+
+const _App = props => {
+  const options = {
+    sidebar: {
+      hidden: process.env.NODE_ENV === "production"
+    }
+  };
+  useEffect(() => {
     const { protocol, hostname, port } = window.location;
     const baseUrl = `${protocol}//${hostname}${
       port != "80" ? `:${port}` : ""
@@ -15,20 +30,13 @@ class MyApp extends App {
 
     const client = new GitClient(baseUrl);
 
-    this.cms.registerApi("git", client);
-  }
-  options = {
-    sidebar: {
-      hidden: process.env.NODE_ENV === "production"
-    }
-  };
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <Tina cms={this.cms} {...this.options.sidebar}>
-        <Component {...pageProps} />
-      </Tina>
-    );
-  }
-}
-export default MyApp;
+    props.cms.registerApi("git", client);
+  }, []);
+
+  const { Component, pageProps } = props;
+  return (
+    <Tina cms={props.cms} {...options.sidebar}>
+      <Component {...pageProps} />
+    </Tina>
+  );
+};
